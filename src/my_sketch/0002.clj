4f9330fe9e91b55e9f035bca861167e92f2486a6
Martin Schenck <martinschenck@fastmail.com>
2018-12-31 13:41:13 +0100

    Initial commit

---
+++ src/my_sketch/0002.clj
@@ -0,0 +1,38 @@
+;; Draws a "circle" of a random color
+(ns my-sketch.0002
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(defn setup []
+  (q/no-loop)
+  (q/color-mode :hsb)
+
+  (q/background 0 0 255)
+  (q/no-stroke)
+  (q/no-fill))
+
+(defn draw []
+  ; Draw a circle manually.
+  (let [center (/ (q/width) 2)
+        radius (/ (q/width) 3)
+        points 9]
+    (q/begin-shape)
+    ; One more than points to close the circle with the last point.
+    (dotimes [i (+ points 1)]
+      (q/fill (rand-int 255) 200 200)
+      (let [alpha (* (/ (* Math/PI 2) points) i)
+            a (* (Math/sin alpha) radius)
+            b (* (Math/cos alpha) radius)
+            x (+ center b)
+            y (+ center a)]
+        (q/vertex x y)))
+    (q/end-shape))
+
+  (q/save "renders/0002.png"))
+
+(q/defsketch my-sketch
+  :title "0002"
+  :size [800 800]
+  ; Setup function called only once, during sketch initialization.
+  :setup setup
+  :draw draw)
